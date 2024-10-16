d007b5a14f573e9d4328da0613d09dec8eafdcaa
Meeraja <147638760+Meeraja-K@users.noreply.github.com>
2024-10-16 22:32:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require('path');
 const app = express();
 
-var publicDir = path.join(__dirname, 'public');
+var publicDir = path.join(__dirname, '.');
 app.use("/", express.static(publicDir));
 
 var nodeModulesDir = path.join(__dirname, 'node_modules');
